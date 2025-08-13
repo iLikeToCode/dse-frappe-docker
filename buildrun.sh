03e5420bea82fbab6d13593c039c5d1f07a63b12
debianbox <root@debianbox>
2025-08-13 16:40:40 +0100

    update build script

--- buildrun.sh
+++ buildrun.sh
@@ -1,4 +1,3 @@
 docker compose -f pwd.yml down --remove-orphans
-docker system prune -a --force
-docker buildx bake --no-cache --set "*.platform=linux/arm64"
+docker buildx bake --set "*.platform=linux/arm64"
 docker compose -f pwd.yml up -d
